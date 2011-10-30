52852c28b047b237143f72b8305bc17cce4086ba
Bartosz Inglot <jhi@o2.pl>
2011-10-30 19:42:20 +0000

    Renamed the class DataCrypter to Dpapi.

--- Bin2Reg/Encoders/Dpapi.cs
+++ Bin2Reg/Encoders/Dpapi.cs
@@ -3,24 +3,24 @@ using System.Security.Cryptography;
 using Bin2Reg.Interfaces;
 
 namespace Bin2Reg.Encoders {
-    public class DataCrypter : IEncoder {
-        public DataCrypter() : this(DataProtectionScope.LocalMachine) { }
+    public class Dpapi : IEncoder {
+        public Dpapi() : this(DataProtectionScope.LocalMachine) { }
 
-        public DataCrypter(DataProtectionScope scope) {
+        public Dpapi(DataProtectionScope scope) {
             ProtectionScope = scope;
         }
 
         public DataProtectionScope ProtectionScope { get; set; }
 
         public byte[] Encode(byte[] plain) {
-            return Dpapi(ProtectedData.Protect, plain);
+            return Encryption(ProtectedData.Protect, plain);
         }
 
         public byte[] Decode(byte[] cipher) {
-            return Dpapi(ProtectedData.Unprotect, cipher);
+            return Encryption(ProtectedData.Unprotect, cipher);
         }
 
-        private byte[] Dpapi(Func<byte[], byte[], DataProtectionScope, byte[]> transform, byte[] data) {
+        private byte[] Encryption(Func<byte[], byte[], DataProtectionScope, byte[]> transform, byte[] data) {
             return transform(data, null, ProtectionScope);
         }
     }
