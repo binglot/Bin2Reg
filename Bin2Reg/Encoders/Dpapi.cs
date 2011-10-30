9d394629c69d3d6ac18499fe7f13b11509ee8285
Bartosz Inglot <jhi@o2.pl>
2011-10-30 19:36:49 +0000

    Added Windows Data Protection API (DPAPI) as another encoder.

---
+++ Bin2Reg/Encoders/Dpapi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using Bin2Reg.Interfaces;
+
+namespace Bin2Reg.Encoders {
+    public class DataCrypter : IEncoder {
+        public DataCrypter() : this(DataProtectionScope.LocalMachine) { }
+
+        public DataCrypter(DataProtectionScope scope) {
+            ProtectionScope = scope;
+        }
+
+        public DataProtectionScope ProtectionScope { get; set; }
+
+        public byte[] Encode(byte[] plain) {
+            return Dpapi(ProtectedData.Protect, plain);
+        }
+
+        public byte[] Decode(byte[] cipher) {
+            return Dpapi(ProtectedData.Unprotect, cipher);
+        }
+
+        private byte[] Dpapi(Func<byte[], byte[], DataProtectionScope, byte[]> transform, byte[] data) {
+            return transform(data, null, ProtectionScope);
+        }
+    }
+}
